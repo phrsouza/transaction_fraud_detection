f7d678c55920f8f6e99c59d38f77cc07ea254b09
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-29 20:06:48 -0300

    create merchant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_225607) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_230134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "merchants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "samples", force: :cascade do |t|
     t.string "title"
