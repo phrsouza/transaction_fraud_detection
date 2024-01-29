733afb7ae244eb16072b0fbbf97200ea514bf242
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-29 20:49:36 -0300

    add transaction association specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Transaction do
+  describe 'associations' do
+    it { is_expected.to belong_to(:device) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:merchant) }
+  end
 end
