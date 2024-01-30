0c3b2e6c0a135cda8d87e3863bc9fb80994c47a0
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-30 08:18:36 -0300

    add env to render compile commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 
-bundle install
-./bin/rails assets:precompile
-./bin/rails assets:clean
+RAILS_ENV=production bundle install
+RAILS_ENV=production ./bin/rails assets:precompile
+RAILS_ENV=production ./bin/rails assets:clean
