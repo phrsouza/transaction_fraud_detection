9e9c15db8a49059cfb13c67a7e0ee09c0dff191d
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-30 02:59:22 -0300

    remove unnecessary timecop usage

--- spec/services/anti_fraud_rules/user_previous_chargeback_spec.rb
+++ spec/services/anti_fraud_rules/user_previous_chargeback_spec.rb
@@ -6,33 +6,29 @@ RSpec.describe AntiFraudRules::UserPreviousChargeback do
   describe '.call' do
     context 'when does not exist a previous chargeback transaction for the user' do
       it 'returns false' do
-        Timecop.freeze do
-          previous_transaction = create(:transaction, chargebacked_at: nil)
+        previous_transaction = create(:transaction, chargebacked_at: nil)
 
-          rule_result = described_class.call(
-            transaction_data: {
-              user_id: previous_transaction.user_id
-            }
-          )
+        rule_result = described_class.call(
+          transaction_data: {
+            user_id: previous_transaction.user_id
+          }
+        )
 
-          expect(rule_result).to be false
-        end
+        expect(rule_result).to be false
       end
     end
 
     context 'when exists a previous chargeback transaction for the user' do
       it 'returns true' do
-        Timecop.freeze do
-          chargebacked_transaction = create(:transaction, :chargebacked)
+        chargebacked_transaction = create(:transaction, :chargebacked)
 
-          rule_result = described_class.call(
-            transaction_data: {
-              user_id: chargebacked_transaction.user_id
-            }
-          )
+        rule_result = described_class.call(
+          transaction_data: {
+            user_id: chargebacked_transaction.user_id
+          }
+        )
 
-          expect(rule_result).to be true
-        end
+        expect(rule_result).to be true
       end
     end
   end
