78729b729ab634d2942b5333ac74296546193c2c
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-30 02:07:50 -0300

    add default relationships to transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     card_number { 'MyString' }
     chargebacked_at { '2024-01-29 20:35:15' }
     amount { '9.99' }
-    device { nil }
-    user { nil }
-    merchant { nil }
+    device
+    user
+    merchant
   end
 end
