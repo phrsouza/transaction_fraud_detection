a2e9f446478378f982a93e5e23edd9a93a497dc4
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-29 22:09:18 -0300

    set device_id as optional in transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
-  belongs_to :device
+  belongs_to :device, optional: true
   belongs_to :user
   belongs_to :merchant
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Transaction do
   describe 'associations' do
-    it { is_expected.to belong_to(:device) }
+    it { is_expected.to belong_to(:device).optional(true) }
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:merchant) }
   end
