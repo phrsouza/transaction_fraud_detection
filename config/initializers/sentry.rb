e9b5ecd9b1c161b2e582710e500aa04709ed7210
Fauno <pedrohenriqueramossouza@gmail.com>
2024-02-03 23:55:37 -0300

    condigure sentry DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-if ENV['SENTRY_DSN']
+Sentry.init do |config|
+  config.dsn = 'https://fe3d5b76e0ae408dd60b23aa6d0aab1e@o4506686246944768.ingest.sentry.io/4506686247927808'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  Sentry.init do |config|
-    config.dsn = ENV['SENTRY_DSN']
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
-    # or
-    config.traces_sampler = lambda do |_context|
-      true
-    end
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
   end
-
 end
